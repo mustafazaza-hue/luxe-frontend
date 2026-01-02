5d8f624fd4a01fbc0c22897f44069cc2a1e4ba2c
mustafazaza-hue <johndoe@example.com>
2026-01-02 04:19:23 +0200

    fixing

--- middleware.js
+++ middleware.js
@@ -1,26 +1,26 @@
-import { NextResponse } from 'next/server';
+// import { NextResponse } from 'next/server';
 
-export function middleware(request) {
-  const token = request.cookies.get('token')?.value;
-  const { pathname } = request.nextUrl;
+// export function middleware(request) {
+//   const token = request.cookies.get('token')?.value;
+//   const { pathname } = request.nextUrl;
 
-  // الصفحات المحمية
-  // const protectedPaths = ['/dashboard', '/account', ];
+//   // الصفحات المحمية
+//   // const protectedPaths = ['/dashboard', '/account', ];
   
-  // // إذا كانت الصفحة محمية ولا يوجد token
-  // if (protectedPaths.some(path => pathname.startsWith(path)) && !token) {
-  //   return NextResponse.redirect(new URL('/login', request.url));
-  // }
+//   // // إذا كانت الصفحة محمية ولا يوجد token
+//   // if (protectedPaths.some(path => pathname.startsWith(path)) && !token) {
+//   //   return NextResponse.redirect(new URL('/login', request.url));
+//   // }
 
-  // إذا كان المستخدم مسجل دخول ويحاول الوصول إلى صفحات التسجيل
-  if ((pathname === '/login' || pathname === '/signup') && token) {
-    return NextResponse.redirect(new URL('/', request.url));
-  }
+//   // إذا كان المستخدم مسجل دخول ويحاول الوصول إلى صفحات التسجيل
+//   if ((pathname === '/login' || pathname === '/signup') && token) {
+//     return NextResponse.redirect(new URL('/', request.url));
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-// تحديد المسارات التي سيتم تطبيق الـ middleware عليها
-export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
-};
+// // تحديد المسارات التي سيتم تطبيق الـ middleware عليها
+// export const config = {
+//   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+// };
